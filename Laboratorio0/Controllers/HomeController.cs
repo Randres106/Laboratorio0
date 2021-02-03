921f47abddbe2607ce063acdf3a79e839c161248
randres <randr@DESKTOP-DVJNG83>
2021-02-02 18:13:45 -0600

    Orden nombre y apellido

--- Laboratorio0/Controllers/HomeController.cs
+++ Laboratorio0/Controllers/HomeController.cs
@@ -45,6 +45,48 @@ namespace Laboratorio0.Controllers
         public ActionResult Privacy()
         {
             return View(Singleton.Instance.ClientList);
+
+        }
+
+        public ActionResult ListName()
+        {
+            List<Customers> NameList = new List<Customers>();
+            for (int i = 0; i < Singleton.Instance.ClientList.Count; i++)
+            {
+                for (int j = 0; j < Singleton.Instance.ClientList.Count-1; j++)
+                {
+                    if (Singleton.Instance.ClientList[j].Name.CompareTo(Singleton.Instance.ClientList[j + 1].Name) > 0)
+                    {
+                        NameList.Add(Singleton.Instance.ClientList[j]);
+                        Singleton.Instance.ClientList[j] = Singleton.Instance.ClientList[j + 1];
+                        Singleton.Instance.ClientList[j + 1] = NameList[0];
+                    }
+                    NameList.Clear();
+                }
+            }
+            
+            return RedirectToAction(nameof(Privacy));
+
+        }
+
+        public ActionResult SurnameList()
+        {
+            List<Customers> SurnameList = new List<Customers>();
+            for (int i = 0; i < Singleton.Instance.ClientList.Count; i++)
+            {
+                for (int j = 0; j < Singleton.Instance.ClientList.Count - 1; j++)
+                {
+                    if (Singleton.Instance.ClientList[j].Surname.CompareTo(Singleton.Instance.ClientList[j + 1].Surname) > 0)
+                    {
+                        SurnameList.Add(Singleton.Instance.ClientList[j]);
+                        Singleton.Instance.ClientList[j] = Singleton.Instance.ClientList[j + 1];
+                        Singleton.Instance.ClientList[j + 1] = SurnameList[0];
+                    }
+                    SurnameList.Clear();
+                }
+            }
+           
+            return RedirectToAction(nameof(Privacy));
         }
         public ActionResult Error()
         {
